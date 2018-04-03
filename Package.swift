c3470bfeb45aab8e0bb65606db5da6e8c3f2c9d5
Jérémie DOCHE <jeremie.doche.dev@outlook.fr>
2018-04-04 01:01:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,18 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0.0
 
 import PackageDescription
 
 let package = Package(
     name: "Blockchain-Server",
-    pkgConfig: nil,
-    providers: nil,
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
+    ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentProvider", "LeafProvider"],
                 exclude: [
@@ -15,43 +22,5 @@ let package = Package(
                 ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
-    ],
-    swiftLanguageVersions: 4,
-// swift-tools-version:3.1
-
-import PackageDescription
-
-let package = Package(
-    name: "Blockchain-Server",
-    pkgConfig: nil,
-    providers: nil,
-    targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "LeafProvider"],
-                exclude: [
-                    "Config",
-                    "Public",
-                    "Resources",
-                    ]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
-        ],
-    swiftLanguageVersions: 4,
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
     ]
 )
-
-
-)
-
