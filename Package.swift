5dfcfd9ca363c8e7b8faf67d2951d18a24fe5359
Jérémie DOCHE <jeremie.doche.dev@outlook.fr>
2018-04-04 00:43:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 let package = Package(
     name: "Blockchain-Server",
     pkgConfig: nil,
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
-    ],
     providers: nil,
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentProvider", "LeafProvider"],
@@ -24,6 +20,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
+    ],
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
     ]
 )
 
